8e88a3c0574112be9e1e0d5e20e2d73c78a41a68
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2020-12-08 13:32:40 +0200

    Updates

---
+++ precache-manifest.60aa5142b9a802ce4b35f24b0800e222.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c436950f773960c0d7c94004ba0a49b5",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "ef1ed134bb0cae398d95",
+    "url": "/Weather-App/static/css/main.cb70381f.chunk.css"
+  },
+  {
+    "revision": "662a85a1adb73e04d27b",
+    "url": "/Weather-App/static/js/2.8c8b0220.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/Weather-App/static/js/2.8c8b0220.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef1ed134bb0cae398d95",
+    "url": "/Weather-App/static/js/main.3299d359.chunk.js"
+  },
+  {
+    "revision": "5b7d0d57f8b0be2f837a",
+    "url": "/Weather-App/static/js/runtime-main.7117aade.js"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/Weather-App/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Weather-App/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a742834a58e74553d141450a7dddf5c0",
+    "url": "/Weather-App/static/media/Rubik-Light.a742834a.ttf"
+  },
+  {
+    "revision": "588d2ce5f23fb14d3feb98fa85f1c36e",
+    "url": "/Weather-App/static/media/backgroundClouds.588d2ce5.jpg"
+  },
+  {
+    "revision": "8a38aafb5eac823630e297e5ff6615ae",
+    "url": "/Weather-App/static/media/glass.8a38aafb.svg"
+  },
+  {
+    "revision": "36e27b4df80e0a89de70c88fb32126a2",
+    "url": "/Weather-App/static/media/profile.36e27b4d.jpg"
+  }
+]);
